24507e50260c8261588c4d8c8320d5cf619abccd
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-02-20 04:51:17 +0000

    Updates display_1

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -4,7 +4,28 @@ class TravelController < ApplicationController
   
   @places = Place.all
 
+  end
+
+  def show
+    # find a Company
+    @places = Place.find_by({"id" => params["id"]})
+    #@contacts = Contact.where({"company_id" => @company["id"]})
+    # render companies/show view with details about Company
+  end
+
+  def new
 
   end
 
+  def create
+    # start with a new Company
+    @Place = Place.new
+    # assign user-entered form data to Company's columns
+    @Place["name"] = params["name"]
+    # save Company row
+    @Place.save
+    # redirect user
+    redirect_to "/places"
+  end
+   
 end
--- create_data.rb
+++ create_data.rb
@@ -16,4 +16,17 @@ Amsterdam = Place.new
 Amsterdam["name"] = "Amsterdam"
 Amsterdam.save
 
+# t.string "title"
+# t.text "description"
+# t.date "posted_on"
+# t.integer "place_id"
+
+Mexico_city_1 = Entry.new
+Mexico_city_1["title"] = "Ate tacos"
+Mexico_city_1["description"] = "It was good but expensive"
+Mexico_city_1["posted_on"] = "02072024"
+Mexico_city_1["place_id"] = Mexico_City["id"]
+Mexico_city_1.save
+
 puts "There are now #{Place.all.count} places"
+puts "There are now #{Entry.all.count} enrties"
