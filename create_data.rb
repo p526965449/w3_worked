cf1b6f85148033c7e4852e00459e050e35350966
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-02-20 01:42:05 +0000

    Update_data

--- create_data.rb
+++ create_data.rb
@@ -1,6 +1,19 @@
 Place.destroy_all
 
-Chennai = Place.new
-Chennai["name"] = "Chennai"
+Mexico_City = Place.new
+Mexico_City["name"] = "Mexico_City"
+Mexico_City.save
+
+Charleston = Place.new
+Charleston["name"] = "Charleston"
+Charleston.save
+
+Beijing = Place.new
+Beijing["name"] = "Beijing"
+Beijing.save
+
+Amsterdam = Place.new
+Amsterdam["name"] = "Amsterdam"
+Amsterdam.save
 
 puts "There are now #{Place.all.count} places"
